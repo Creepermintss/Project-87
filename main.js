30736b01ed45574ebda2bcd4d307f5607c55da6a
Creepermintss <144182795+Creepermintss@users.noreply.github.com>
2023-10-05 18:07:15 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -10,9 +10,9 @@ with 430 width and 200 height.
 */
 
 ctx.beginPath();
-ctx.strokeStyle = "grey";
-ctx._linewidth = 1;
-ctx.rect()(150, 143, width, height);
+ctx.color = "grey";
+ctx.lineWidth= 1;
+ctx.rect(150, 143, 430, 200);
 ctx.stroke();
 
 /*
@@ -26,25 +26,31 @@ with 40 as radius, 0 startAngle and 2 * Math.PI endAngle.
 ctx.beginPath();
 ctx.strokeStyle = "blue";
 ctx.lineWidth = 5;
-ctx.arc()(250, 210, 40, 0, 2*MathPI);
+ctx.arc(250, 210, 40, 0,2 *Math.PI);
 ctx.stroke();
 
 // Similarly, create a black circle with position 350 and 210
 ctx.beginPath();
-ctx.strokeStyle = "black";
+ctx.strokeStyle= "black";
 ctx.lineWidth = 5;
-ctx.arc()(250, 210, 40, 0, 2*MathPI);
+ctx.arc(350, 210, 40, 0,2 *Math.PI);
 ctx.stroke();
+
 // Similarly, create a red circle with position 210 and 40
 ctx.beginPath();
 ctx.strokeStyle = "red";
 ctx.lineWidth = 5;
-ctx.arc()(250, 210, 40, 0, 2*MathPI);
+ctx.arc(450, 210, 40, 0,2 *Math.PI);
 ctx.stroke();
 // Similarly, create an orange circle with position 300 and 250
 ctx.beginPath();
 ctx.strokeStyle = "orange";
 ctx.lineWidth = 5;
-ctx.arc()(250, 210, 40, 0, 2*MathPI);
+ctx.arc(300, 250, 40, 0,2 *Math.PI);
 ctx.stroke();
 // Similarly, create a green circle with position 400 and 250
+ctx.beginPath();
+ctx.strokeStyle = "green";
+ctx.lineWidth = 5;
+ctx.arc(400, 250, 40, 0,2 *Math.PI);
+ctx.stroke();
